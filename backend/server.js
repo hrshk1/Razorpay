9410ccd76baa6d2592951eaf9b21a531ce0d1781
hrshk1 <hrsh.110070391@gmail.com>
2026-01-11 18:23:54 +0530

    chore: configure server to use environment variables for port

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import app from './app.js';
-
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+import dotenv from 'dotenv';
+dotenv.config({ path: "backend/config/config.env" });
+const port=process.env.PORT||3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
