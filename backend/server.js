2cf4fc8c65d2ab5a94b16e2938a8863cee844b32
hrshk1 <hrsh.110070391@gmail.com>
2026-01-11 22:24:01 +0530

    chore: implement Razorpay payment processing and add setup instructions to notes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,19 @@ import dotenv from 'dotenv';
 import Razorpay from 'razorpay';
 dotenv.config({ path: "backend/config/config.env" });
 const port=process.env.PORT||3000;
+
+export const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_API_SECRET,
+});
+
+const processPayment = async(req,res)=>{
+    res.status(200).json({
+        success: true,
+    })
+}
+app.route('/payment/process').post(processPayment);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
