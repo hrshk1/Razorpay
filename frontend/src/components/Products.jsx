a09535e6032ba9d5c788af89fea73ec6f370ca76
hrshk1 <hrsh.110070391@gmail.com>
2026-01-12 16:04:11 +0530

    feat: add product data structure for dynamic product rendering

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -9,24 +9,6 @@ const Products = () => {
         <p className='product-price'>Price<strong>200</strong></p>
         <button className='pay-button'>Pay(200)/-</button>
     </div>
-    <div className="product-card">
-        <img src="./images/bag.jpg" alt="Product" className='product-image'/>
-        <h3>Product Title</h3>
-        <p className='product-price'>Price<strong>200</strong></p>
-        <button className='pay-button'>Pay(200)/-</button>
-    </div>
-    <div className="product-card">
-        <img src="./images/bag.jpg" alt="Product" className='product-image'/>
-        <h3>Product Title</h3>
-        <p className='product-price'>Price<strong>200</strong></p>
-        <button className='pay-button'>Pay(200)/-</button>
-    </div>
-    <div className="product-card">
-        <img src="./images/bag.jpg" alt="Product" className='product-image'/>
-        <h3>Product Title</h3>
-        <p className='product-price'>Price<strong>200</strong></p>
-        <button className='pay-button'>Pay(200)/-</button>
-    </div>
    </div>
   )
 }
---
+++ frontend/src/components/data.js
@@ -0,0 +1,27 @@
+const productData=[
+    {
+    id:1,
+    title:"Soft Bag",
+    proce:600,
+    image:"./images/bag.jpg"
+    },
+     {
+    id:2,
+    title:"Headphone",
+    proce:600,
+    image:"./images/headphone.jpg"
+    },
+     {
+    id:3,
+    title:"Jacket",
+    proce:600,
+    image:"./images/jacket.jpg"
+    },
+     {
+    id:4,
+    title:"Jeans",
+    proce:600,
+    image:"./images/jeans.jpg"
+    },
+]
+export default productData;
