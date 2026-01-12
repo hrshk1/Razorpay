91374adee1476cd9913479eb5c9d848e1bfcc2dd
hrshk1 <hrsh.110070391@gmail.com>
2026-01-12 15:57:14 +0530

    Add Products component and styles, and create PaymentSuccess styles
    - Implemented a new Products component to display product cards with images, titles, prices, and payment buttons.
    - Added corresponding CSS styles for the Products component to enhance layout and design.
    - Created a new CSS file for PaymentSuccess styles to improve the user experience after a successful payment.

---
+++ frontend/src/components/Products.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../styles/products.css';
+const Products = () => {
+  return (
+   <div className="products-container">
+    <div className="product-card">
+        <img src="./images/bag.jpg" alt="Product" className='product-image'/>
+        <h3>Product Title</h3>
+        <p className='product-price'>Price<strong>200</strong></p>
+        <button className='pay-button'>Pay(200)/-</button>
+    </div>
+    <div className="product-card">
+        <img src="./images/bag.jpg" alt="Product" className='product-image'/>
+        <h3>Product Title</h3>
+        <p className='product-price'>Price<strong>200</strong></p>
+        <button className='pay-button'>Pay(200)/-</button>
+    </div>
+    <div className="product-card">
+        <img src="./images/bag.jpg" alt="Product" className='product-image'/>
+        <h3>Product Title</h3>
+        <p className='product-price'>Price<strong>200</strong></p>
+        <button className='pay-button'>Pay(200)/-</button>
+    </div>
+    <div className="product-card">
+        <img src="./images/bag.jpg" alt="Product" className='product-image'/>
+        <h3>Product Title</h3>
+        <p className='product-price'>Price<strong>200</strong></p>
+        <button className='pay-button'>Pay(200)/-</button>
+    </div>
+   </div>
+  )
+}
+
+export default Products
