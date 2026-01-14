c41df137e8c3dd8bb08f542c02c8d1798bb3b83e
hrshk1 <hrsh.110070391@gmail.com>
2026-01-14 15:06:11 +0530

    feat:verification backend route added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 import {instance} from '../server.js';
+import crypto from 'crypto';
 export const processPayment = async(req,res)=>{
     const options={
         amount:Number(req.body.amount*100),
@@ -15,4 +16,20 @@ export const getKey = async(req,res)=>{
     res.status(200).json({
         key: process.env.RAZORPAY_API_KEY,
     })
+}
+
+//payment verification
+export const paymentVerification = async(req,res)=>{
+    const {razorpay_payment_id,razorpay_order_id,razorpay_signature}=req.body;
+    const body=razorpay_order_id+"|"+razorpay_payment_id;
+    const expectedSignature=crypto.createHmac("sha256",process.env.RAZORPAY_API_SECRET).update(body.toString()).digest("hex");
+    const isAuthentic=expectedSignature===razorpay_signature;
+    if(isAuthentic){
+        return res.redirect(`http://localhost:5173/payment/success?reference=${razorpay_payment_id}`);
+    }
+    else{
+        return res.status(400).json({
+            success:false,
+        });
+    }
 }
